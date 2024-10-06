fe97cfbb2ffab327c8ddbc085af3fda104e8e420
manh <nguyenducmanh100619dta@gmail.com>
2024-10-06 09:24:17 +0700

    init: Create order food project

---
+++ src/main/kotlin/com/project/OrderFood/OrderFoodApplication.kt
@@ -0,0 +1,11 @@
+package com.project.OrderFood
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OrderFoodApplication
+
+fun main(args: Array<String>) {
+	runApplication<OrderFoodApplication>(*args)
+}
